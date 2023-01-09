e5964f8ad6f3e1cbed5d95d316b1577dbe63a419
nizaralloul7 <nizaralloul7@outlook.fr>
2023-01-09 21:46:52 +0100

    heroku

--- src/main/java/com/example/agentservice/controller/AgentController.java
+++ src/main/java/com/example/agentservice/controller/AgentController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/agent")
+@CrossOrigin(origins = "*")
 public class AgentController
 {
     private final AgentService agentService;
